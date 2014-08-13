4b06298833651fcc144286f9ca36cc6fb4937b37
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-13 14:20:26 -0500

    Problem 2

--- if_and_or.php
+++ if_and_or.php
@@ -9,3 +9,19 @@
  if ($x < $y && $y < $z) {
  	echo "{$x} < {$y} < {$z}\n";
  }
+
+ // if $x is greater than 0 OR less than 10, echo the result as a sentence "$x is greater than 0 OR less than 10".
+
+if ($x > 0 || $x < 10) {
+	echo "$x is greater than 0 OR less than 10\n";
+}
+
+ // repeat the if statement for $y and $z.
+
+if ($y > 0 || $y < 10) {
+	echo "$y is greater than 0 OR less than 10\n";
+}
+
+if ($z > 0 || $z < 10) {
+	echo "$z is greater than 0 OR less than 10\n";
+}
