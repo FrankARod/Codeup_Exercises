25316ab6b0606cfeb5490513d48b49df0019b0e5
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-15 10:32:55 -0500

    Added exercises 2 and 3

--- foreach.php
+++ foreach.php
@@ -29,6 +29,28 @@ foreach ($data as $datum) {
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 foreach ($things as $type) {
 	if (is_numeric($type)) {
+		echo "{$type} is an integer\n";
+	} elseif (is_float($type)){
+		echo "{$type} is a float\n";
+	} elseif (is_bool($type)) {
+		echo "{$type} is a boolean\n";
+	} elseif (is_array($type)) {
+		echo print_r($type) . " is an array\n";
+	} elseif (is_null($type)) {
+		echo "{$type} is null\n";
+	} else/* (is_string($type))*/ {
+		echo "{$type} is a string\n";
+	}
+}
+
+foreach ($things as $type) {
+	if (is_scalar($type)) {
+		echo "{$type}\n";
+	}
+}
+
+foreach ($things as $type) {
+	if (is_numeric($type)) {
 		echo "{$type}\n";
 	} elseif (is_float($type)){
 		echo "{$type}\n";
