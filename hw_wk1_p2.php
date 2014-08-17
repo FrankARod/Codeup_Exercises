73d4c41bcc0d463da19350889eea49c9aca99279
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-17 18:08:42 -0500

    Outputs a quiz from nested arrays

---
+++ hw_wk1_p2.php
@@ -0,0 +1,33 @@
+<?php
+
+$quiz = ['question 1' => array('question' => 'Did this question print?',
+							   'answers' => array('a' => 'No',
+							   					  'b' => 'Yeah',
+							   					  'c' => 'Sorta',
+							   					  'd' => 'Uhhh'),
+							   'correct_answer' => 'b'),
+		 'question 2' => array('question' => 'Who is the President of the United States',
+							   'answers' => array('a' => 'Barack Obama',
+							   					  'b' => 'Arnold Schwarzenegger',
+							   					  'c' => 'Ron Paul',
+							   					  'd' => 'Richard Stallman'),
+							   'correct_answer' => 'a'),
+		 'question 3' => array('question' => 'What is 1 + 3?',
+							   'answers' => array('a' => '5',
+							   					  'b' => '8',
+							   					  'c' => '4',
+							   					  'd' => 'U'),
+							   'correct_answer' => 'c')];
+$question_num = 1;
+foreach($quiz as $question) {
+echo "$question_num. {$question['question']}\n\n";
+	foreach ($question['answers'] as $letter => $answer) {
+		if ($letter != $question['correct_answer']) {
+			echo "\t$letter. {$answer}\n";
+		} else {
+			echo "*\t$letter. {$answer}\n";
+		  }
+	}
+		 	// a. {$question['answers']['a']}\nb. {$question['answers']['b']}\nc. {$question['answers']['c']}\nd. {$question['answers']['d']}\n";
+$question_num++;
+}
