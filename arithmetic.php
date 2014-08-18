01234374d8dc8555df918751c274ef3f17030fff
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-18 14:55:47 -0500

    Added modulus function

--- arithmetic.php
+++ arithmetic.php
@@ -1,19 +1,29 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+    echo $a + $b . PHP_EOL;
 }
 
 function subtract($a, $b) {
-	echo $a - $b;
+	echo $a - $b . PHP_EOL;
 }
 
 function multiply($a, $b) {
-    echo $a * $b;
+    echo $a * $b . PHP_EOL;
 }
 
 function divide($a, $b) {
-    echo $a / $b;
+    echo $a / $b . PHP_EOL;
 }
+
+function modulus($a, $b) {
+	echo $a % $b . PHP_EOL;
+}
+
+add(3, 5);
+subtract(5, 4);
+multiply(1, 7);
+divide(6, 3);
+modulus(6, 2);
 
 ?>
