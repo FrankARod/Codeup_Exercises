947fb386b7d38192c2a0e36118549ae3d178a09b
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-18 15:39:23 -0500

    Added error messages

--- arithmetic.php
+++ arithmetic.php
@@ -1,29 +1,60 @@
 <?php
 
+
+
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+    if (is_numeric($a) && is_numeric($b)){
+    	echo $a + $b . PHP_EOL;	
+    } else {
+    	echo "ERROR: Please enter only numeric arguments." . PHP_EOL;
+    }
 }
 
 function subtract($a, $b) {
-	echo $a - $b . PHP_EOL;
+	 if (is_numeric($a) && is_numeric($b)){
+    	echo $a - $b . PHP_EOL;
+    } else {
+    	echo "ERROR: Please enter only numeric arguments." . PHP_EOL;
+    }
+
 }
 
 function multiply($a, $b) {
-    echo $a * $b . PHP_EOL;
+    if (is_numeric($a) && is_numeric($b)){
+    	echo $a * $b . PHP_EOL;
+    } else {
+    	echo "ERROR: Please enter only numeric arguments." . PHP_EOL;
+    }
 }
 
 function divide($a, $b) {
-    echo $a / $b . PHP_EOL;
+    if (is_numeric($a) && is_numeric($b)){
+    	if ($b == 0) {
+    		echo "ERROR: Please don't divide by zero." . PHP_EOL;
+    	} else {
+    	echo $a / $b . PHP_EOL;
+    	}
+    } else {
+    	echo "ERROR: Please enter only numeric arguments." . PHP_EOL;
+    }
 }
 
 function modulus($a, $b) {
-	echo $a % $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+    	if ($b == 0) {
+    		echo "ERROR: Please don't divide by zero." . PHP_EOL;
+    	} else {
+    		echo $a % $b . PHP_EOL;
+    	}	
+    } else {
+    	echo "ERROR: Please enter only numeric arguments." . PHP_EOL;
+    }
 }
 
 add(3, 5);
-subtract(5, 4);
-multiply(1, 7);
-divide(6, 3);
+subtract(5, 'fore');
+multiply(1, '7');
+divide(6, 0);
 modulus(6, 2);
 
 ?>
