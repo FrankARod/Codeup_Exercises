440389c65091cd89ef31aac75a6e9e347d37f42a
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-18 14:51:24 -0500

    Completed functions

--- arithmetic.php
+++ arithmetic.php
@@ -5,15 +5,15 @@ function add($a, $b) {
 }
 
 function subtract($a, $b) {
-    // Add code here
+	echo $a - $b;
 }
 
 function multiply($a, $b) {
-    // Add code here
+    echo $a * $b;
 }
 
 function divide($a, $b) {
-    // Add code here
+    echo $a / $b;
 }
 
 ?>
