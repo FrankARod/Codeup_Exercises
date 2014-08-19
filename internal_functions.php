dbb96cb176bfbaf274193521e76053c95736faf8
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-19 11:56:09 -0500

    Relaced echos with returns in functions

--- internal_functions.php
+++ internal_functions.php
@@ -1,19 +1,39 @@
 <?php
 
-<?php
-
 $nothing = NULL;
 $something = '';
 $array = array(1,2,3);
 
 // Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
 
+function is_empty($var) {
+	if (isset($var)/* || empty($var)*/) {
+		return '$var is SET' . PHP_EOL;
+		// return true;
+	} elseif (empty($var)) {
+		return '$var is EMPTY' . PHP_EOL;
+		// return false;
+	}
+}
+
+
 // TEST: If var $nothing is set, display '$nothing is SET'
+
+echo is_empty($nothing);
 
 // TEST: If var $nothing is empty, display '$nothing is EMPTY'
 
 // TEST: If var $something is set, display '$something is SET'
 
+
+echo is_empty($something);
+
 // Serialize the array $array, and output the results
 
+$string_array = serialize($array);
+echo $string_array . PHP_EOL;
+
 // Unserialize the array $array, and output the results
+
+$restored_array = unserialize($string_array);
+echo var_dump($restored_array);
