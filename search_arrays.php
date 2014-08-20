a184e0f38bb4f61d2537dbf916507f4ac17a2a4e
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-20 11:07:50 -0500

    Added function to find amount of elements in common between arrays

--- search_arrays.php
+++ search_arrays.php
@@ -14,5 +14,16 @@ function result_found($query, $array) {
  	}
  }
 
+function values_in_common($array1, $array2) {
+	$elements_in_common = 0;
+	for ($i = 0; $i < count($array1); $i++) {
+		if (result_found($array1[$i], $array2)) {
+			$elements_in_common++;
+		}
+	}
+	return $elements_in_common;	
+}
+
 var_dump(result_found('Tina', $names));
 var_dump(result_found('Bob', $names));
+echo values_in_common($names, $compare) . PHP_EOL;
