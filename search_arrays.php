8f467fb60421c83b17d780f67562dbf5dd8aa261
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-20 10:55:30 -0500

    Created function to output if result is found

--- search_arrays.php
+++ search_arrays.php
@@ -1,6 +1,18 @@
 <?php
 
- // first names
- $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+// first names
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
- $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+function result_found($query, $array) {
+ 	$result = array_search($query, $array);
+ 	if ($result === FALSE) {
+ 		return FALSE;
+ 	} else {
+ 		return TRUE;
+ 	}
+ }
+
+var_dump(result_found('Tina', $names));
+var_dump(result_found('Bob', $names));
